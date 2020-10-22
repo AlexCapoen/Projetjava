5ecaafd25c98622e9753cf401498f9c7ece44039
AlexCapoen <alex.capoen@hotmail.fr>
2020-10-22 11:44:14 +0200

    client init

--- bar/client.java
+++ bar/client.java
@@ -1,10 +1,20 @@
 class Client extends Humain{
+
+//__________________Init________________________________________________________
+
   private boissonFav;
   private boissonFavSecours;
   private degresAlcool;
   private presentationComplementaire;
 
+  public Client(String boissonFav,String boissonFavSecours,double degresAlcool,String presentationComplementaire){
+    this.boissonFav = boissonFav;
+    this.boissonFavSecours = boissonFavSecours;
+    this.degresAlcool = degresAlcool;
+    this.presentationComplementaire = presentationComplementaire;
+  }
+
   public void seFaireOfrrirUnVerre(){
-    
+
   }
 }
